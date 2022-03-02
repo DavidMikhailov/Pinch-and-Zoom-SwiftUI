b0ac8d95a7244b780533036a2a683eac86f57e59
David Mikhailov <david.mikhailovv@gmail.com>
2022-03-02 18:01:45 +0400

    Initial commit

---
+++ Pinch & Zoom/Pinch_ZoomApp.swift
@@ -0,0 +1,18 @@
+//
+//  Pinch_Zoom.swift
+//  Restart
+//
+//  Created by Давид Михайлов on 02.03.2022.
+//
+
+
+import SwiftUI
+
+@main
+struct Pinch_Zoom: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch & Zoom/Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch & Zoom
+//
+//  Created by Давид Михайлов on 02.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
